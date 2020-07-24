230c9bb7207b226c99a5395f8fc3bae9f3bae0a4
dguis <dallin.guisti@dodecabyte.com>
2020-07-24 09:01:56 -0700

    Initialize Plugin

---
+++ src/main/java/com/dodecabyte/worlds/worldmanager/WorldManager.java
@@ -0,0 +1,17 @@
+package com.dodecabyte.worlds.worldmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WorldManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
